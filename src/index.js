4df11813b9f27e278b06804d73c6857235f92dfc
khaledk8 <khalidkari1@gmail.com>
2024-05-23 22:44:27 +0300

    edited header height and added logic to home

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import styleFooter from './footer'
 const content = document.getElementById('content')
 content.classList.add('p-7', 'flex', 'justify-center', 'items-center')
 
+const homeButton = document.getElementById('home')
+const menuButton = document.getElementById('menu')
+
 const body = document.body
 body.style.backgroundImage = `url(${myImage})`
 body.classList.add('flex', 'flex-col', 'gap-20', 'justify-between')
@@ -14,3 +17,10 @@ body.classList.add('flex', 'flex-col', 'gap-20', 'justify-between')
 styleNav()
 styleFooter()
 content.appendChild(addHome())
+
+homeButton.addEventListener('click', () => {
+    content.removeChild(content.firstChild)
+    content.appendChild(addHome())
+})
+
+
